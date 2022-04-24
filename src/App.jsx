61ca3ea340c6e2886b87cef40a80bf31b76dced0
Sangimi <hsi3020@naver.com>
2022-04-24 21:31:36 +0900

    input componet map

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,78 @@
 import "./App.css";
-import { useRef } from "react";
+import { useState } from "react";
 import FormInput from "./componetn/FormInput";
 
 function App() {
-  // const [username, setUsername] = useState("");  //이렇게 했을때, 계속해서 기입할때마다 재랜더링이 된다.
-  // const usernameRef = useRef();
+  const [values, setValues] = useState({
+    username: "",
+    email: "",
+    birthday: "",
+    password: "",
+    confirmPassword: "",
+  }); //이렇게 했을때, 계속해서 기입할때마다 재랜더링이 된다.
+
+  const inputs = [
+    {
+      id: 1,
+      name: "username",
+      type: "text",
+      placeholder: "Username",
+      label: "Username",
+    },
+    {
+      id: 2,
+      name: "email",
+      type: "text",
+      placeholder: "Email",
+      label: "Email",
+    },
+    {
+      id: 3,
+      name: "birthday",
+      type: "text",
+      placeholder: "Birthday",
+      label: "Birthday",
+    },
+
+    {
+      id: 4,
+      name: "password",
+      type: "password",
+      placeholder: "Password",
+      label: "Password",
+    },
+    {
+      id: 5,
+      name: "confirmPassword",
+      type: "password",
+      placeholder: "ConfirmPassword",
+      label: "ConfirmPassword",
+    },
+  ];
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    const data = new FormData(e.target);
-    console.log(Object.fromEntries(data.entries()));
   };
+
+  const onChange = (e) => {
+    setValues({ ...values, [e.target.name]: e.target.value });
+  };
+
+  console.log(values);
 
   return (
     <div className="App">
       <form onSubmit={handleSubmit}>
-        <FormInput name="username" placeholder="Username" />
-        <FormInput name="email" placeholder="Email" />
-        <FormInput name="fullname" placeholder="Full name" />
-        <FormInput name="sht" placeholder="Sth else" />
+        <h1>Register</h1>
+        {inputs.map((input) => (
+          <FormInput
+            key={input.id}
+            {...input}
+            value={values[input.name]}
+            onChange={onChange}
+          />
+        ))}
+
         <button>Submit</button>
       </form>
     </div>
